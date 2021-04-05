9ffad4c9aab8358d7c5a766397fef5f75f2235b9
Yuri Filippov <yefilippov@gmail.com>
2021-04-05 21:43:55 +0300

    Initial Commit

---
+++ Middle5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Middle5
+//
+//  Created by Юрий Филиппов on 05.04.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
